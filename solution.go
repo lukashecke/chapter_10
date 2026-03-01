0c65231161359bea495b7afd0bdcb27a3f31ef9e
Lukas Hecke <info@lukashecke.com>
2026-03-01 23:08:55 +0100

    Add code doc

--- solution.go
+++ solution.go
@@ -1,5 +1,9 @@
+// Package chapter10 contains the solution to the exercises in chapter 10 of Learning Go, 2nd edition.
 package chapter10
 
+// Adds two integer values and returns their sum as defined in [mathisfun].
+//
+// [mathisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
